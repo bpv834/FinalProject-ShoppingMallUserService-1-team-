ea3dc03c3805e4f3bff8ae91993f917b871da976
ho <yungjoon2@naver.com>
2025-01-13 14:12:21 +0900

    FruMe Home Commit

--- app/src/main/java/com/example/frume/HomeActivity.kt
+++ app/src/main/java/com/example/frume/HomeActivity.kt
@@ -4,6 +4,6 @@ class HomeActivity {
 }
 // 프래그먼트들을 나타내는 값들
 enum class HomeFragmentName(var number:Int, var str:String){
-    // 게시판 메인 화면
+    // 홈 메인 화면
     HOME_MAIN_FRAGMENT(1, "HomeMainFragment"),
 }
