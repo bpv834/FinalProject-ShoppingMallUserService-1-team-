85759d86ae1d0c077b3759b13b9fa358324802b4
zzangse1 <zzangse1014@gmail.com>
2025-01-15 13:16:39 +0900

    sehoon ui done

---
+++ app/src/main/java/com/example/frume/fragment/user_fragment/product_info/UserProductInfoReviewFragment.kt
@@ -0,0 +1,23 @@
+package com.example.frume.fragment.user_fragment.product_info
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.frume.R
+
+
+class UserProductInfoReviewFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_user_product_info_review, container, false)
+    }
+
+
+}
