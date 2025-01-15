a50260273595e690c02ad43ba7fb458a72f269f1
bpv834 <bpv8342Gmail.com>
2025-01-15 13:12:59 +0900

    my info ui 구현 완료

---
+++ app/src/main/java/com/example/frume/fragment/home_fragment/my_info/UserPwModifyFragment.kt
@@ -0,0 +1,20 @@
+package com.example.frume.fragment.home_fragment.my_info
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.frume.R
+
+class UserPwModifyFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_user_pw_modify, container, false)
+    }
+
+}
